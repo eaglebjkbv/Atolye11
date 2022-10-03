d526b08ad4ee8c8f65060f733fa8eaf48279aabe
VIYANA <bbvardal@gmail.com>
2022-10-03 15:06:13 +0300

    Sept 3 some exercises added

---
+++ If2/If2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /* 
+            * Ekranda 
+            * 1.Elma
+            * 2.Armut
+            * 3.Kiraz
+            * 4.Karpuz
+            * Lüfen bir Meyve Seçiniz....
+            * Seçime Göre Hangi meyveyi seçtiğini ekranda yazılacak. 
+        */
+        {
+            while (true) { 
+            Console.WriteLine("1. Elma");
+            Console.WriteLine("2. Armut");
+            Console.WriteLine("3. Kiraz");
+            Console.WriteLine("4. Karpuz");
+            Console.Write("Lütfen bir meyve seçiniz.(1..4).:");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (secim == 1)
+            {
+                Console.WriteLine("Elma seçildi");
+                Console.WriteLine("Elmalar rendelenir, sonra hamura karıştırılır.");
+            }else if (secim == 2)
+            {
+
+                Console.WriteLine("Armut Seçildi...");
+            }else if (secim == 3)
+            {
+                Console.WriteLine("Kiraz Seçildi...");
+            }else if (secim == 4)
+            {
+                Console.WriteLine("Karpuz Seçildi...");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen 1 ile 4 arası bir sayı giriniz..");
+            }
+            Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Switch2/Switch2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     1. Üçgen alanı");
+            Console.WriteLine("     2. Daire Alanı");
+            Console.WriteLine("     3. Diktörgen alanı");
+            Console.WriteLine("----------------------------------");
+            Console.Write("Lütfen Seçimimini Giriniz (1-2) :");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            switch (sec)
+            {
+                case 1:
+                    Console.Write("Taban Uzunluğu Giriniz. ");
+                    int t = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Yüksekliği giriniz.. :");
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    int alan = t * h / 2;
+                    Console.WriteLine("Alan ={0} cm2", alan);
+                    break;
+                case 2:
+                    Console.Write("Yarıçapı Giriniz. :");
+                    int r = Convert.ToInt32(Console.ReadLine());
+                    float alanD = r * r * 3.14f;
+                    Console.WriteLine($"Dairenin Alanı ={alanD} cm2");
+                    break;
+                case 3:
+                    Console.WriteLine("A Kenarını Giriniz .:");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("B Kenarını Giriniz");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    int alanDiktortgen = a * b;
+                    if (a != b)
+                    {
+                        Console.WriteLine($"Diktörgenin alanı ={alanDiktortgen}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Karenin alanı ={alanDiktortgen}");
+
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Lütfen 1 ya da 2 Girniz...");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
