aa771caa11de4c4630f60b4bad13e75c8b727bb9
VIYANA <bbvardal@gmail.com>
2022-10-04 16:06:47 +0300

    While loop example added

---
+++ HelloWhile/HelloWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            int toplam = 0;
+            while (sayi < 10)
+            {
+                sayi++;
+                Console.WriteLine(sayi);
+                toplam += sayi;
+            }
+            Console.WriteLine($"Toplam ={toplam}");
+            Console.ReadKey();
+        }
+    }
+}
