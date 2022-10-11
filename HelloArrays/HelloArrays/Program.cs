ad1918318ad61f48b59427a4d92ac726dca5daae
VIYANA <bbvardal@gmail.com>
2022-10-11 17:00:45 +0300

    Arrays examples added

---
+++ HelloArrays/HelloArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            int[] sayilar = new int[100];
+            for(int i = 0; i < 100; i++)
+            {
+                sayilar[i] = rastgele.Next(1, 100);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i} inci sayı :"+ sayilar[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringArrays/StringArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arabalar = {"BMW","Mercedes","Audi","Porsche","Togg","volksWagen","Tesla" };
+            Console.WriteLine($"Listede {arabalar.Length} adet araba vardır");
+            for (int i = 0; i < arabalar.Length; i++)
+            {
+                Console.WriteLine(arabalar[i]);
+
+            }
+            Console.WriteLine("----------- Sıralı Liste ------------");
+            Array.Sort(arabalar);
+            for (int i = 0; i < arabalar.Length; i++)
+            {
+                Console.WriteLine(arabalar[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
