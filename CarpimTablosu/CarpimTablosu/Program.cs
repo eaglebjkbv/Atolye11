cc2f69c6f66d0574b2b1e461abc9adaf7d11e59a
VIYANA <bbvardal@gmail.com>
2022-10-11 11:47:05 +0300

    multiplication table example added

---
+++ CarpimTablosu/CarpimTablosu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpimTablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{i}x{j}={i*j}\t");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
