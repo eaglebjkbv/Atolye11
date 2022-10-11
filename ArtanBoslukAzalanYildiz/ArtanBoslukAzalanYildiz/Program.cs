430d98e8f3794e8b9b978d32e70843c87b7bd8fa
VIYANA <bbvardal@gmail.com>
2022-10-11 11:28:23 +0300

    New star examples added

---
+++ ArtanBoslukAzalanYildiz/ArtanBoslukAzalanYildiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtanBoslukAzalanYildiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *****
+            //  ****
+            //   ***
+            //    **
+            //     *
+            // Yukarıdaki Şekli çizdiren programı yazınız.
+            for(int i = 1; i < 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int a = 0; a < 6 - i; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
