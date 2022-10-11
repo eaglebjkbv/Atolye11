d2235a1c35c99cc66b8b9f267987903f1c998338
VIYANA <bbvardal@gmail.com>
2022-10-11 09:53:14 +0300

    New star example added

---
+++ ArtanYildiz/ArtanYildiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtanYildiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *
+            // ** 
+            // ***
+            // ****
+            // *****
+            // Yukarıdaki şekli ekrana çizdiren progamı yapınız.
+            for(int i = 1; i < 6; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
