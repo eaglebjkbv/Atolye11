ca27f21f63d0594dfc9b29cbae386f1649702dd7
VIYANA <bbvardal@gmail.com>
2022-10-11 11:27:10 +0300

    new star exaples added

---
+++ AzalanBoslukArtanYildiz/AzalanBoslukArtanYildiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzalanBoslukArtanYildiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //      *
+            //     **
+            //    ***
+            //   ****
+            //  *****
+            // Yukarıdaki Şekli Çizen programı yazınız...
+
+            for (int i = 1; i < 6; i++)
+            {
+                for(int j = 0; j < 6 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int a = 0; a < i; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
