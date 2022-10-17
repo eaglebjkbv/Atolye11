27c70802c45aca670df9f01020acf9d030a401ee
VIYANA <bbvardal@gmail.com>
2022-10-17 10:09:58 +0300

    new star examples added

---
+++ DuzPiramit/DuzPiramit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuzPiramit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   *
+            //  ***
+            // *****
+            //*******
+            // Yukarıdaki şekli çizdiren programı yazınız...
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 1; j < 4 - i; j++)
+                {
+                    Console.Write("-");
+                }
+                for(int z = 0; z < i * 2 + 1; z++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
