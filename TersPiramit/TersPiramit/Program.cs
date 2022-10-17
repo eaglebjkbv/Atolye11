deb739f5057c5d9772c5ff2672ae5b0e89348cfc
VIYANA <bbvardal@gmail.com>
2022-10-17 10:47:42 +0300

    new phyramit example added

---
+++ TersPiramit/TersPiramit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TersPiramit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *******
+            //  *****
+            //   ***
+            //    *
+            // Yukarıdaki şekli çizdiren programı yazınız.
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int z = 0; z < (4 - i) * 2 - 1; z++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
